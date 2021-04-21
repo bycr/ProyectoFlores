368a41583b8770f0f56224fade929574b2d4d368
Brayan Cañon <brayan13canon@gmail.com>
2021-04-21 18:33:48 -0500

    cambios

--- Proyectoflores/App_Start/FilterConfig.cs
+++ Proyectoflores/App_Start/FilterConfig.cs
@@ -8,7 +8,7 @@ namespace Proyectoflores
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            //filters.Add(new Filters.VerificaSession());
+            filters.Add(new Filters.VerificaSession());
         }
     }
 }
