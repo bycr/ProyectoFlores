671de68207074b1ab5284264c21e040a5dcf4785
Brayan Cañon <brayan13canon@gmail.com>
2021-04-25 00:05:51 -0500

    Recursos multi idioma

--- Proyectoflores/Recursos/Recurso.Designer.cs
+++ Proyectoflores/Recursos/Recurso.Designer.cs
@@ -19,7 +19,7 @@ namespace Recursos {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Recurso {
@@ -57,6 +57,126 @@ namespace Recursos {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Bloques.
+        /// </summary>
+        public static string _Layout_Bloques {
+            get {
+                return ResourceManager.GetString("_Layout_Bloques", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Fertirriego.
+        /// </summary>
+        public static string _Layout_Fertirriego {
+            get {
+                return ResourceManager.GetString("_Layout_Fertirriego", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Fincas.
+        /// </summary>
+        public static string _Layout_Fincas {
+            get {
+                return ResourceManager.GetString("_Layout_Fincas", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Índice.
+        /// </summary>
+        public static string _Layout_inicio {
+            get {
+                return ResourceManager.GetString("_Layout_inicio", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Invernaderos.
+        /// </summary>
+        public static string _Layout_Invernaderos {
+            get {
+                return ResourceManager.GetString("_Layout_Invernaderos", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Producción.
+        /// </summary>
+        public static string _Layout_Produccion {
+            get {
+                return ResourceManager.GetString("_Layout_Produccion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Usuarios.
+        /// </summary>
+        public static string _Layout_Usuarios {
+            get {
+                return ResourceManager.GetString("_Layout_Usuarios", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Este sistema de inventario esta desarrollado único y exclusivamente para los
+        ///propietarios y administradores de las empresas de flores, si usted por algún motivo
+        ///logro acceder sin hacer parte de alguno de estos dos últimos a los cuales se hace
+        ///referencia, por favor salga del sistema y no haga mal uso de esta aplicación web..
+        /// </summary>
+        public static string Home_parrafo {
+            get {
+                return ResourceManager.GetString("Home_parrafo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Bienvenido al sistema de información para la producción de flor..
+        /// </summary>
+        public static string Home_parrafo1 {
+            get {
+                return ResourceManager.GetString("Home_parrafo1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a SISTEMA DE INFORMACIÓN PRODUCCIÓN DE FLOR.
+        /// </summary>
+        public static string Home_Titulo {
+            get {
+                return ResourceManager.GetString("Home_Titulo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a acceder.
+        /// </summary>
+        public static string Login_boton_acceso {
+            get {
+                return ResourceManager.GetString("Login_boton_acceso", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Contraseña.
+        /// </summary>
+        public static string Login_contraseña {
+            get {
+                return ResourceManager.GetString("Login_contraseña", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a usuario.
+        /// </summary>
+        public static string Login_usuario {
+            get {
+                return ResourceManager.GetString("Login_usuario", resourceCulture);
             }
         }
     }
